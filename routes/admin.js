840ead5a7dfee46f488a67d873cd77846e719ed9
Viktor Palyuha <viktorpalyuha@gmail.com>
2021-01-30 19:09:01 +0200

    pug briefly learned

--- routes/admin.js
+++ routes/admin.js
@@ -3,13 +3,16 @@ const path = require('path');
 const router = express.Router();
 const rootDir = require('../util/path');
 
+const products = [];
+
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product'});
 });
 
 router.post('/add-product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
+  products.push({ title: req.body.title });
+  res.redirect('/');
 });
 
-module.exports = router;
+module.exports.routes = router;
+module.exports.products = products;
