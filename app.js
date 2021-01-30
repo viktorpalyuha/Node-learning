342c228f98e850af62a6e80fdc3427a2877550f3
Viktor Palyuha <viktorpalyuha@gmail.com>
2021-01-30 15:09:06 +0200

    express learned

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 
-const routes = require('./routes');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-const server = http.createServer(routes);
+const app = express();
 
-server.listen(3000);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((_, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(3000);
